77300f5a2ffbb1dbc70df258642b59b6cb97b333
8100d1r0n <8100d1r0n@riseup.net>
2016-08-24 03:43:57 +0300

    Encrypting shell scripts

---
+++ enc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#########################################################################
+# Author: 8100d1r0n
+# E-mail: echo  ODEwMGQxcjBuKGF0KXJpc2V1cChkb3QpbmV0Cg==|base64 -d
+# Created Time: 2016-08-24T03:23:20+0300
+# File Name: enc.sh
+#########################################################################
+function usage() {
+    echo -e "Usage:\n./`basename $0` -e ScriptFile CreateEncryptFileName"
+    echo -e "./`basename $0` -r RunEncrytFileName"
+}
+if [ -z "$1" ]
+then
+    usage
+    exit 1;
+fi
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -r|--run)
+            openssl enc -d -aes-256-cbc -a -in "$2" | sh -
+            exit 1;
+            ;;
+        -e|--encrypt)
+            openssl enc -e -aes-256-cbc -a -in "$2" -out "$3"
+            exit 1;
+            ;;
+        -h|--help)
+            usage
+            exit 1;
+            ;;
+        esac
+    done
